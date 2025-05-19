68faf5362836b69730533a35f7af8aaa35eb3c0c
Jacob Smith <129335957+jacobsmithdev@users.noreply.github.com>
2025-05-19 15:45:02 -0600

    feat: Add class to store and manipulate datasets
    Add a class for storing and manipulating an array of objects of a
    specific class.
    
    The Dataset class relies on DataType having a unique attribute called
    'id'. Some problems arise from this: for instance, a Person class may
    have its own 'id' attribute which conflicts with how Dataset uses the
    property.
    
    There are ways around this, such as wrapping each object in a new
    'DataRow' class and setting a unique index/id attribute there. Given the
    scope and intent of the project however, I have decided it is not worth
    addressing for the time being.
    
    note: findIndex() returns -1 when it can't find an item, which splice()
    treats as a valid, negative index. To prevent the last array item from
    being removed, check if index is valid before calling splice().

---
+++ src/modules/classes/Dataset.js
@@ -0,0 +1,63 @@
+export default class Dataset {
+    #items;
+    #DataType;
+
+    constructor(Datatype, items = []) {
+        this.#DataType = Datatype;
+        this.#items = items;
+    }
+
+    get items() {
+        return this.#items;
+    }
+
+    get DataType() {
+        return this.#DataType;
+    }
+
+    create(...args) {
+        // If a full object of the correct type is passed in, 
+        // push it instead of creating one from scratch
+        if (args[0] instanceof this.DataType) {
+            const item = args[0];
+            this.#items.push(item)
+            return item;
+        }
+
+        const item = new this.DataType(...args);
+        this.#items.push(item);
+        return item;
+    }
+
+    read(id) {
+        const item = this.#items.find(item => item.id === id);
+        if (item) return item;
+        return null;
+    }
+
+    update(id, field, value) {
+        const item = this.get(id).item;
+        item[field] = value;
+        return item;
+    }
+
+    delete(id) {
+        const itemIndex = this.#items.findIndex(item => item.id === id);
+    
+        // findIndex() returns -1 when it can't find an item, which 
+        // splice() treats as a valid, negative index. To prevent the 
+        // last array item from being removed, check if index is valid 
+        // before calling splice().
+        const isValidIndex = itemIndex > -1;
+        if (isValidIndex) {
+            const deletedItem = this.#items[itemIndex];
+            this.#items.splice(itemIndex, 1);
+            return deletedItem;
+        };
+        return null;
+    }
+
+    query(queryFn) {
+        return this.#items.filter(item => queryFn(item));
+    }
+}
