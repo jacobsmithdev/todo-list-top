941e9e528def902c3ef844d6a8f2f3ab10cf5a51
Jacob Smith <129335957+jacobsmithdev@users.noreply.github.com>
2025-05-20 09:19:09 -0600

    feat: Add class to represent todo query
    Add class to store todo query and described it in human-readable terms,
    which will be important for anything UI related.

---
+++ src/modules/classes/TodoQuery.js
@@ -0,0 +1,8 @@
+export default class TodoQuery {
+    constructor(query, title, description, projectId) {
+        this.query = query;
+        this.title = title;
+        this.description = description;
+        this.projectId = projectId;
+    }
+}
