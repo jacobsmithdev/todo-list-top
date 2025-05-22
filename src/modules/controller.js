3b5e5344c6e0942368b7c764edbd715bf1641dd2
Jacob Smith <129335957+jacobsmithdev@users.noreply.github.com>
2025-05-22 14:48:24 -0600

    feat: Add 'active' data management to controller
    Add a set of variables and functions which control 'active' data, or
    the data which should be displayed in the UI/DOM.

--- src/modules/controller.js
+++ src/modules/controller.js
@@ -1,5 +1,47 @@
 import database from "./database.js";
 import observer from "./observer.js";
+import TodoQuery from "./classes/TodoQuery.js";
+import Project from "./classes/Project.js";
+
+let activeQuery = null;
+let activeTodos = [];
+
+function setActiveQuery(input) {
+    if (input instanceof TodoQuery) {
+        const query = input;
+        activeQuery = query;
+        updateActiveTodos();
+        observer.publish('activeQueryUpdate');
+        return true;
+    }
+
+    // If a project is passed in, create a query
+    // to get all todos corresponding to that project
+    if (input instanceof Project) {
+        const project = input;
+        const queryFn = todo => todo.projectId === project.id;
+        activeQuery = new TodoQuery(queryFn, project.title, project.description, project.id);
+        updateActiveTodos();
+        observer.publish('activeQueryUpdate');
+        return true;
+    }
+    return false;
+}
+
+function getActiveQuery() {
+    return activeQuery;
+}
+
+function updateActiveTodos() {
+    if (activeQuery) {
+        activeTodos = database.todos.query(activeQuery.query);
+    }
+    observer.publish('activeTodosUpdate');
+}
+
+function getActiveTodos() {
+    return activeTodos;
+}
 
 function queryTodos(query) {
     observer.publish('todoQuery');
@@ -56,6 +98,9 @@ export default {
     getAllProjects,
     addProject,
     removeProject,
+    getActiveTodos,
+    getActiveQuery,
+    setActiveQuery,
 }
 
 export {
