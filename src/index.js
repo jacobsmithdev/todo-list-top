4fa9770b962efc3013e39080c7ec0ffcd33e671d
Jacob Smith <129335957+jacobsmithdev@users.noreply.github.com>
2025-05-24 13:09:06 -0600

    feat: Add test data/methods for project
    Add some temporary code to index.js for testing purposes. This includes:
    
     - A set of template projects/todos
     - Access to relevant methods from the window object
     - Logs for event publishing
     - Setting activeQuery to a default value

--- src/index.js
+++ src/index.js
@@ -1,2 +1,42 @@
 import './reset.css';
 import './index.css';
+
+import controller from "./modules/controller";
+import observer from "./modules/observer.js";
+import testData from "./modules/testData.js";
+import TodoQuery from "./modules/classes/TodoQuery.js";
+
+const [projects, todos] = testData.createProjectArray(5);
+projects.forEach(project => controller.addProject(project));
+todos.forEach(todo => controller.addTodo(todo));
+
+window.controller = controller;
+window.observer = observer;
+window.testData = testData;
+window.TodoQuery = TodoQuery;
+
+observer.subscribe('todoQuery', () => {
+    console.log('todoQuery');
+})
+
+observer.subscribe('todoUpdate', () => {
+    console.log('todoUpdate');
+})
+
+observer.subscribe('projectQuery', () => {
+    console.log('projectQuery');
+})
+
+observer.subscribe('projectUpdate', () => {
+    console.log('projectUpdate');
+})
+
+observer.subscribe('activeQueryUpdate', () => {
+    console.log('activeQueryUpdate');
+})
+
+observer.subscribe('activeTodosUpdate', () => {
+    console.log('activeTodosUpdate');
+})
+
+controller.setActiveQuery(projects[0]);
