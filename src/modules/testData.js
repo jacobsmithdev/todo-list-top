61d2916e0b2425c200bc8bcfd63fe2bdf802fd89
Jacob Smith <129335957+jacobsmithdev@users.noreply.github.com>
2025-07-07 14:46:52 -0600

    feat: Configure testData to create todos with actual date properties
    Generate a random date and time for each todo instead of using the
    string '00-00-0000'.

--- src/modules/testData.js
+++ src/modules/testData.js
@@ -6,12 +6,37 @@ const CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let todoCounter = 0;
 let projectCounter = 0;
 
+function generateRandomDate() {
+    const today = new Date(Date.now());
+
+    const year = String(today.getFullYear()).padStart(2, "0");
+
+    // Months are zero based in date objects - increment to display "correct" month
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+
+    const daysOffset = Math.floor(Math.random() * 7);
+    const days = String(today.getDate() + daysOffset).padStart(2, "0");
+
+    const hours = String(Math.floor(Math.random() * 23)).padStart(2, "0");
+
+    const minutesMultiple = 5;
+    const randomMinutes = Math.floor(Math.random() * 59);
+    // Floor randomMinutes to the closest multiple of minutesMultiple
+    const minutesAmount = Math.floor(randomMinutes / minutesMultiple) * minutesMultiple;
+    const minutes = String(minutesAmount).padStart(2, "0");
+
+    const dateString = `${year}-${month}-${days}T${hours}:${minutes}:00`;
+    const randomDate = new Date(dateString);
+    
+    return randomDate;
+}
+
 function createTodo(projectId = null) {
     const title = `todo${todoCounter.toString().padStart(3, '0')}`;
     const description = getRandomString(10);
     const priority = Math.floor(Math.random() * 5) + 1;
     const completed = Math.random() < 0.5;
-    const date = '00-00-0000';
+    const date = generateRandomDate();
 
     const todo = new Todo(title, description, date, priority, completed, projectId);
     todoCounter++;
